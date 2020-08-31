e8d21e71ada4a18e964bdd1ea180b65dd82cc334
bewuwy <bewuwy@gmail.com>
2020-08-31 08:23:15 +0200

    initial commit

---
+++ auto-fav.py
@@ -0,0 +1,14 @@
+from ppadb.client import Client
+from PIL import Image
+import numpy
+import time
+
+adb = Client(host='127.0.0.1', port=5037)
+devices = adb.devices()
+
+if len(devices) == 0:
+    print('no device attached')
+    quit()
+
+device = devices[0]
+
